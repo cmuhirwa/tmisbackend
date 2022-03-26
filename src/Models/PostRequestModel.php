c8b051422549df45bb770097dfd41a9e1f085d42
mugishajc <patrick_ntwari@yahoo.com>
2022-03-26 12:44:16 +0200

    changed positions API to add qualifications dynamicaly

--- src/Models/PostRequestModel.php
+++ src/Models/PostRequestModel.php
@@ -35,11 +35,11 @@ class PostRequestModel {
     public function getSchoolRequestsPerDistrict($academicId, $districtId)
     {
         $statement = " 
-        SELECT q.qualification_name, p.position_name, s.schoolname, pr.post_request_id, pr.academic_calendar_id, pr.school_code, pr.position_code, pr.qualification_id, pr.head_teacher_id, pr.head_teacher_post_request, pr.head_teacher_reason_id, pr.dde_id_request, pr.dde_post_request, pr.dde_id_distribution, pr.dde_post_distribution, pr.dde_distribution_comment, pr.district_code, pr.created_by
+        SELECT q.qualification_name, p.position_name, s.school_name, pr.post_request_id, pr.academic_calendar_id, pr.school_code, pr.position_code, pr.qualification_id, pr.head_teacher_id, pr.head_teacher_post_request, pr.head_teacher_reason_id, pr.dde_id_request, pr.dde_post_request, pr.dde_id_distribution, pr.dde_post_distribution, pr.dde_distribution_comment, pr.district_code, pr.created_by
         FROM post_request pr
         INNER JOIN qualifications q ON pr.qualification_id = q.qualification_id
         INNER JOIN positions p ON pr.position_code = p.position_code
-        INNER JOIN schools s ON pr.school_code = s.schoolcode
+        INNER JOIN schools s ON pr.school_code = s.school_code
       
         WHERE academic_calendar_id = ? AND district_code = ? ";
 
