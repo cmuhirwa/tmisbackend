46b982379c0014e089892a44741a2e0d27c58a1b
mugishajc <patrick_ntwari@yahoo.com>
2022-03-26 15:16:00 +0200

    I removed requested posts from the list of selection'

--- src/Models/PositionModel.php
+++ src/Models/PositionModel.php
@@ -12,19 +12,45 @@ class PositionModel {
 
     public function findAll()
     {
+
       $statement = " 
-      SELECT p.position_id, p.position_code, p.position_name, sl.school_level_name, p.qualification_id, q.qualification_name
-      FROM positions p  
-      INNER JOIN qualifications q ON p.qualification_id = q.qualification_id
-      INNER JOIN school_levels sl ON p.school_level_code = sl.school_level_code
+        SELECT p.position_id, p.position_code, p.position_name, sl.school_level_name, p.qualification_id, q.qualification_name
+        FROM positions p  
+        INNER JOIN qualifications q ON p.qualification_id = q.qualification_id
+        INNER JOIN school_levels sl ON p.school_level_code = sl.school_level_code
       ";
       try {
           $statement = $this->db->query($statement);
-          $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+          $positionsArray = $statement->fetchAll(\PDO::FETCH_ASSOC);
+          $result = $this->removeSelectedPositions($positionsArray);
           return $result;
       } catch (\PDOException $e) {
           exit($e->getMessage());
       }
+    }
+
+    public function removeSelectedPositions($positionsArray)
+    {
+      $result = [];
+      foreach($positionsArray as $positionsObject){
+        $position_code = $positionsObject['position_code'];
+        $statement = " 
+            SELECT * FROM post_request WHERE position_code = ?
+          ";
+          try {
+              $statement = $this->db->prepare($statement);
+              $statement->execute(array($position_code));
+              $post_request_that_has_this_position_array = $statement->fetchAll(\PDO::FETCH_ASSOC);
+              if(sizeof($post_request_that_has_this_position_array) == 0){
+                array_push($result, $positionsObject);
+              }
+              
+          } catch (\PDOException $e) {
+              exit($e->getMessage());
+          }
+
+      }
+      return $result;
     }
 
     public function findOne($position_id)
